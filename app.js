d2897e01f6c0d38667c6f9474d89b4a389e01c58
Zorigt <zorigt@gmail.com>
2016-11-02 12:51:24 -0700

    hash map test

--- app.js
+++ app.js
@@ -275,34 +275,7 @@ function receivedMessage(event) {
     //temp.push(quickReplyPayload);
 
     
-    rs.set({
-      app: rsapp,
-      token: temp,
-      d: {
-        "unread_msgs": 12,
-        "last_action": "/read/news",
-        "birthday": "2013-08-13"
-      }},
-      function(err, resp) {
-        /*
-        resp contains the session with the new values:
-     
-        {  
-          "id":"user1001",
-          "r": 1,
-          "w": 2,
-          "idle": 1,
-          "ttl": 7200, 
-          "d":
-            {
-              "foo": "bar",
-              "unread_msgs": 12,
-              "last_action": "/read/news",
-              "birthday": "2013-08-13"
-            }
-        }
-        */  
-    });
+    
     secondAry.push(quickReplyPayload);
 
     if(quickReplyPayload == 'happy') {
@@ -472,7 +445,7 @@ function sendPrimaryFeelings(recipientId) {
       function(err, resp) {
         // resp should be something like  
         // {token: "r30kKwv3sA6ExrJ9OmLSm4Wo3nt9MQA1yG94wn6ByFbNrVWhcwAyOM7Zhfxqh8fe"} 
-        temp = resp.token;
+        //temp = resp.token;
         console.log("Resp token = " + resp.token);
   });
 
